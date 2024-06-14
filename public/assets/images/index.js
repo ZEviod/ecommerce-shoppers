e42a73039a9f000694b6d553cc1cd8a5ef9e6fe9
ViveK Poonia <vivekpoonia2112@gmail.com>
2024-06-14 12:12:17 +0530

    main

---
+++ public/assets/images/index.js
@@ -0,0 +1,31 @@
+import logo from "./logo.png";
+import phoneImg from "./phoneImg.webp";
+import sliderImgOne from "./slider/sliderImgOne.webp";
+import sliderImgTwo from "./slider/sliderImgTwo.webp";
+import sliderImgThree from "./slider/sliderImgThree.webp";
+import sliderImgFour from "./slider/sliderImgFour.webp";
+import sliderImgFive from "./slider/sliderImgFive.webp";
+import bannerImg from "./slider/bannerImg.webp";
+import ship1Img from "./ship/ship1.png";
+import ship2Img from "./ship/ship2.png";
+import ship3Img from "./ship/ship3.png";
+import notFoundImg from "./404Img.png";
+import emptyCart from "./empty-cart.png";
+import warningImg from "./warning.png";
+
+export {
+  logo,
+  phoneImg,
+  sliderImgOne,
+  sliderImgTwo,
+  sliderImgThree,
+  sliderImgFour,
+  sliderImgFive,
+  bannerImg,
+  ship1Img,
+  ship2Img,
+  ship3Img,
+  notFoundImg,
+  emptyCart,
+  warningImg,
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,41 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    maxWidth: {
+      container: "1520px",
+      contentContainer: "1280px",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280",
+        "2xl": "1400px",
+      },
+      colors: {
+        blue: "#0071dc",
+        lightBlue: "#e6f1fc",
+        yellow: "#ffc220",
+        hoverBg: "#004f9a",
+        lightText: "#46474a",
+      },
+      boxShadow: {
+        bannerShadow: "0 1px 2px 1px #00000026",
       },
     },
   },
   plugins: [],
 };
-export default config;
